b77dfebd9e8e78f1d27f970bb6ce02a5765fb493
Phil Miller <phill.c.miller@gmail.com>
2020-08-03 18:06:47 -0500

    cleanup database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -11,9 +11,7 @@ import (
 
 // Initialize initializes the dynamo client
 func Initialize() *dynamodb.DynamoDB {
-	// Initialize a session that the SDK will use to load
-	// credentials from the shared credentials file ~/.aws/credentials
-	// and region from the shared configuration file ~/.aws/config.
+	// Create Session
 	log.Println("Creating Session")
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -23,6 +21,7 @@ func Initialize() *dynamodb.DynamoDB {
 	log.Println("Create DynamoDB Client...")
 	var svc *dynamodb.DynamoDB
 	if os.Getenv("AWS_SAM_LOCAL") == "true" {
+		// Use local endpoint if invoked from SAM
 		log.Println("Local invoke detected; Using local resources")
 		localCfg := aws.Config{
 			Endpoint: aws.String("http://172.16.123.1:8000"),
@@ -31,10 +30,6 @@ func Initialize() *dynamodb.DynamoDB {
 	} else {
 		svc = dynamodb.New(sess)
 	}
-
-	// Debug
-	log.Println("endpoint: ", svc.Endpoint)
-	log.Println("signing region: ", svc.SigningRegion)
 
 	// Create Prices Table
 	CreatePricesTable(svc)
